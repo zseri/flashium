98d12fcdc73ae51cf15a09d77f385e1532371af6
zseri <zseri.devel@ytrizja.de>
2021-03-10 03:09:11 +0100

    fix error 'undefined variable suffix'; refactor translate

--- flake.nix
+++ flake.nix
@@ -10,45 +10,41 @@
     in {
       overlay = final: prev: (
         let
-          translate = (name:
-            let
-              newchrom = nixpkgs.${name}.override { enablePepperFlash = true; };
-              lndir = final.lndir;
-            in final.stdenvNoCC.mkDerivation {
-              pname = name + suffix;
-              version = nixpkgs.${name}.version;
-              nativeBuildInputs = [ lndir ];
-              buildInputs = [ chrom ];
-              buildCommand = ''
-                mkdir -p $out/share
-                mkdir $out/share/applications $out/share/icons
+          translate = (pname: newchrom: final.stdenvNoCC.mkDerivation {
+            inherit pname;
+            inherit (newchrom) version;
 
-                for i in 'bin ' 'share/man/man1 .1'; do
-                  tmpmid=$(echo "$i"| cut -f1 -d' ')
-                  tmpsfx=$(echo "$i"| cut -f2 -d' ')
-                  mkdir -p $out/$tmpmid
-                  ln -s "${newchrom}/$tmpmid/${oldname}$tmpsfx" "$out/$tmpmid/${newname}$tmpsfx"
-                done
+            nativeBuildInputs = [ final.lndir ];
+            buildInputs = [ newchrom ];
+            buildCommand = ''
+              mkdir -p $out/share
+              mkdir $out/share/applications $out/share/icons
 
-                ${lndir}/bin/lndir -silent ${newchrom}/share/icons $out/share/icons
-                for i in $out/share/icons/*/*; do
-                  mv -T "$i" "''${i/${oldname}/${newname}}"
-                done
+              for i in 'bin ' 'share/man/man1 .1'; do
+                tmpmid=$(echo "$i"| cut -f1 -d' ')
+                tmpsfx=$(echo "$i"| cut -f2 -d' ')
+                mkdir -p $out/$tmpmid
+                ln -s "${newchrom}/$tmpmid/${oldname}$tmpsfx" "$out/$tmpmid/${newname}$tmpsfx"
+              done
 
-                cp -T ${newchrom}/share/applications/${oldname}-browser.desktop $out/share/applications/${newname}-browser.desktop
-                substituteInPlace $out/share/applications/${newname}-browser.desktop \
-                  --replace ${oldname}-browser ${namename}-browser \
-                  --replace ${oldname} ${newname} \
-                  --replace Chromium Flashium
-              '';
-            }
-          );
+              ${final.lndir}/bin/lndir -silent ${newchrom}/share/icons $out/share/icons
+              for i in $out/share/icons/*/*; do
+                mv -T "$i" "''${i/${oldname}/${newname}}"
+              done
+
+              cp -T ${newchrom}/share/applications/${oldname}-browser.desktop $out/share/applications/${newname}-browser.desktop
+              substituteInPlace $out/share/applications/${newname}-browser.desktop \
+                --replace ${oldname}-browser ${namename}-browser \
+                --replace ${oldname} ${newname} \
+                --replace Chromium Flashium
+            '';
+          });
         in
           attrsets.listToAttrs (
             builtins.map
-              (n: {
+              (n: rec {
                 name = n + newname;
-                value = translate (n + oldname);
+                value = translate name (nixpkgs.${n + oldname}.override { enablePepperFlash = true; });
               })
               pwl
           )
