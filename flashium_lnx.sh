f79c7e028654687db2225cfc7384748ff9cd31ed
zseri <zseri.devel@ytrizja.de>
2021-03-12 23:30:07 +0100

    flashium_lnx.sh: normalize input path

--- flashium_lnx.sh
+++ flashium_lnx.sh
@@ -2,11 +2,8 @@
 
 # USAGE: flashium_lndir.sh PATH_FROM_CHROMIUM
 export PATH=''
-trg="$(echo "$1" | '@gnused@/bin/sed' -e "s#@newchrom@#$out#" -e 's#chromium#flashium#')"
-if [ "$1" == "$trg" ]; then
-  echo "skip $*"
-  exit 0
-fi
-echo "$1 -> $trg"
+src="$(@coreutils@/realpath "$1")"
+trg="$(echo "$src" | '@gnused@/bin/sed' -e "s#@newchrom@#$out#" -e 's#chromium#flashium#')"
+echo "$src -> $trg"
 @coreutils@/bin/mkdir -p "$(@coreutils@/bin/dirname "$trg")"
-@coreutils@/bin/ln -s "$1" "$trg"
+@coreutils@/bin/ln -s "$src" "$trg"
