054cb7553365fbeeb2db3726d0c7e4f59e0ee97f
zseri <zseri.devel@ytrizja.de>
2021-03-13 00:26:25 +0100

    flashium_lnx.sh: constitent handling of $out/$trg

--- flashium_lnx.sh
+++ flashium_lnx.sh
@@ -3,8 +3,7 @@
 # USAGE: flashium_lndir.sh PATH_FROM_CHROMIUM
 export PATH='@coreutils@/bin'
 src="$(realpath "$1")"
-trg="$(echo "$1" | @gnused@/bin/sed -e 's#chromium#flashium#' -e "s#^\.#$PWD#" -e 's#@newchrom@/##')"
-[ "${trg:0:1}" == / ] || trg="$out/$trg"
+trg="$out/$(echo "$1" | @gnused@/bin/sed -e 's#chromium#flashium#' -e "s#^\.#$PWD#" -e 's#@newchrom@/##')"
 echo "$1 -> $src -> $trg"
-mkdir -p "$out/$(dirname "$trg")"
-ln -s "$src" "$out/$trg"
+mkdir -p "$(dirname "$trg")"
+ln -s "$src" "$trg"
