b0e9675ab598773ae0b63317d9b31b76693f5046
zseri <zseri.devel@ytrizja.de>
2021-03-13 00:30:27 +0100

    split sed exprs into multiple parts to ease debugging

--- flashium_lnx.sh
+++ flashium_lnx.sh
@@ -3,7 +3,13 @@
 # USAGE: flashium_lndir.sh PATH_FROM_CHROMIUM
 export PATH='@coreutils@/bin'
 src="$(realpath "$1")"
-trg="$out/$(echo "$1" | @gnused@/bin/sed -e 's#chromium#flashium#' -e "s#^\.#$PWD#" -e 's#@newchrom@/##')"
+trg="$(echo "$1" | @gnused@/bin/sed -e 's#chromium#flashium#')"
+echo "trgi[0]: $trg"
+trg="$(echo "$trg" | @gnused@/bin/sed -e "s#^\.#$PWD#")"
+echo "trgi[1]: $trg"
+trg="$(echo "$trg" | @gnused@/bin/sed -e 's#@newchrom@/##')"
+echo "trgi[2]: $trg"
+trg="$out/$trg"
 echo "$1 -> $src -> $trg"
 mkdir -p "$(dirname "$trg")"
 ln -s "$src" "$trg"
