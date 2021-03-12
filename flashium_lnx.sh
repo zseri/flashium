6f5b858461a8e485694117f6d444a900672b034e
zseri <zseri.devel@ytrizja.de>
2021-03-12 23:23:10 +0100

    catch useless flashium_lnx.sh calls

--- flashium_lnx.sh
+++ flashium_lnx.sh
@@ -2,8 +2,11 @@
 
 # USAGE: flashium_lndir.sh PATH_FROM_CHROMIUM
 export PATH=''
-echo "$*"
 trg="$(echo "$1" | '@gnused@/bin/sed' -e "s#@newchrom@#$out#" -e 's#chromium#flashium#')"
+if [ "$1" == "$trg" ]; then
+  echo "skip $*"
+  exit 0
+fi
 echo "$1 -> $trg"
 @coreutils@/bin/mkdir -p "$(@coreutils@/bin/dirname "$trg")"
 @coreutils@/bin/ln -s "$1" "$trg"
