69d8befd71ba6ca2cf8465d26e9ee8aa5e3dcc61
zseri <zseri.devel@ytrizja.de>
2021-03-13 00:07:02 +0100

    try again to fix path handling in flashium_lnx.sh

--- flashium_lnx.sh
+++ flashium_lnx.sh
@@ -2,8 +2,8 @@
 
 # USAGE: flashium_lndir.sh PATH_FROM_CHROMIUM
 export PATH='@coreutils@/bin'
-src="$(realpath --relative-base="@newchrom@" "$1")"
-trg="$(echo "$src" | @gnused@/bin/sed -e 's#chromium#flashium#')"
+src="$(realpath "$1")"
+trg="$(echo "$1" | @gnused@/bin/sed -e "s#^\.#$PWD#" -e 's#@newchrom@/##' -e 's#chromium#flashium#')"
 [ "${trg:0:1}" == / ] || trg="$(realpath "$out/$trg" )"
 echo "$src -> $trg"
 mkdir -p "$out/$(dirname "$trg")"
