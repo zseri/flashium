1aebcb5afc095581d329b16b4512eb4b99f076f9
zseri <zseri.devel@ytrizja.de>
2021-03-13 00:20:47 +0100

    flashium_lnx.sh: fix sed expression order

--- flashium_lnx.sh
+++ flashium_lnx.sh
@@ -3,7 +3,7 @@
 # USAGE: flashium_lndir.sh PATH_FROM_CHROMIUM
 export PATH='@coreutils@/bin'
 src="$(realpath "$1")"
-trg="$(echo "$1" | @gnused@/bin/sed -e "s#^\.#$PWD#" -e 's#@newchrom@/##' -e 's#chromium#flashium#')"
+trg="$(echo "$1" | @gnused@/bin/sed -e 's#chromium#flashium#' -e "s#^\.#$PWD#" -e 's#@newchrom@/##')"
 [ "${trg:0:1}" == / ] || trg="$out/$trg"
 echo "$src -> $trg"
 mkdir -p "$out/$(dirname "$trg")"
