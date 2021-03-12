cfd84f52d0fa945822aa2d56d1d7969981baa63a
zseri <zseri.devel@ytrizja.de>
2021-03-13 00:22:18 +0100

    flasium_lnx.sh: debug: print argument

--- flashium_lnx.sh
+++ flashium_lnx.sh
@@ -5,6 +5,6 @@ export PATH='@coreutils@/bin'
 src="$(realpath "$1")"
 trg="$(echo "$1" | @gnused@/bin/sed -e 's#chromium#flashium#' -e "s#^\.#$PWD#" -e 's#@newchrom@/##')"
 [ "${trg:0:1}" == / ] || trg="$out/$trg"
-echo "$src -> $trg"
+echo "$1 -> $src -> $trg"
 mkdir -p "$out/$(dirname "$trg")"
 ln -s "$src" "$out/$trg"
